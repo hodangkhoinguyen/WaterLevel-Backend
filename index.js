9a4528f6bed100012dbf23a134f2b452299e8128
hodangkhoinguye <50374959+hodangkhoinguyen@users.noreply.github.com>
2022-05-28 19:56:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+// This is our main server file
+
+// include express
+const express = require("express");
+// create object to interface with express
+const app = express();
+
+// Code in this section sets up an express pipeline
+
+// print info about incoming HTTP request 
+// for debugging
+app.use(function(req, res, next) {
+  console.log(req.method,req.url);
+  next();
+})
+
+// No static server or /public because this server
+// is only for AJAX requests
+
+// respond to all AJAX querires with this message
+app.use(function(req, res, next) {
+  req.json({msg: "No such AJAX request"})
+});
+
+// end of pipeline specification
+
+// Now listen for HTTP requests
+// it's an event listener on the server!
+const listener = app.listen(3000, function () {
+  console.log("The static server is listening on port " + listener.address().port);
+});
